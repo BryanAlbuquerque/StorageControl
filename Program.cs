37177b15a3ed9505cb9cea76fade9921f98b9552
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-08-28 12:21:33 -0300

    Renomeado Proojeto

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace StorageProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TelaPrincipal());
+            Application.Run(new LoginForm());
         }
     }
 }
