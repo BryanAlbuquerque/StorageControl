fbe17acf3989d167954de16e0c2a43154b726cb3
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-09-08 12:10:50 -0300

    Alterações

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -276,7 +276,7 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.BorderRadius = 15;
+            this.btnLogin.BorderRadius = 10;
             this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLogin.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btnLogin.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
